2b92f1dbbbf9ea8325ab73b4d5bfcd40fcd01117
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-04-03 17:20:16 +0200

    feat(pars): add syntactic parsing for message

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,9 +1,13 @@
 #ifndef PARSER_HPP
 # define PARSER_HPP
+# include "Message.hpp"
+# include <stdexcept>
+# include <string>
 
-class Parser 
+namespace Parser 
 {
-    
+    void    parse(const std::string& rawMessage, Message& msg);
+    void    extractParams(std::string line, Message& msg);
 };
 
 #endif

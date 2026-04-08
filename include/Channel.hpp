c5e9da366512128f5c683074636c91e40a85abc3
Milos Mitkovic <mitkovic_m@hotmail.com>
2026-04-08 13:31:06 +0200

    Add Channel class

---
+++ include/Channel.hpp
@@ -0,0 +1,40 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+//#include "Client.hpp"
+#include <string>
+#include <vector>
+
+class Client;
+
+class Channel
+{
+private:
+  std::string           _channel_name;
+  Client                *_admin;
+  std::vector<Client*>  _clients_list;
+  /* Channel MODEs */
+  bool                  _i; // Set/remove Invite-only
+  bool                  _t; // Set/remove the restrictions of the TOPIC command to channel operators
+  std::string           _k; // Set/remove the channel key (password)
+  unsigned int          _l; // Set/remove the user limit to channel
+  // Give/take channel operator privilege
+
+public:
+  Channel(std::string& name, std::string& key, Client *admin);
+  ~Channel();
+
+  void add_client(Client*);
+  void remove_client(Client*);
+  /* 1. Some sort of broadcast message to notify
+   * all clients that someone joined (_clinets_list)*/
+  /* 2. general broadcast message in the channel */
+
+  // --- get-set
+  void setKey(std::string&);
+  std::string getKey() const;
+  void setMaxMembers(unsigned int);
+  unsigned int getMaxMembers() const;
+};
+
+#endif
